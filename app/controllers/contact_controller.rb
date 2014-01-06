0ab8bcdab973fcac03ba2246ce9fc518095ae63b
saasbook <naingyeaung@berkeley.edu>
2014-01-06 09:00:08 -0800

    changed bootstrap nav bar color

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,7 @@
 class ContactController < ApplicationController
 	def email
 		@email = params[:content]
+		@address = params[:senders_email]
         UserMailer.email("xx", "xx", @email).deliver
         flash[:notice]= "Email has been sent. We will reply to you soon."
         redirect_to "/contact"
